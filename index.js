498c22af888db63ea77d53c2fa679fa7461b43b0
Sourav Nandi <sn9378115@gmail.com>
2025-01-31 12:19:19 +0530

    changes

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-const italic=document.querySelector("i")
-let word="BharatCart"
-let characterIndex=0
-let reverseType=false
+const italic = document.querySelector("i");
+let word = "BharatCart.";
+let characterIndex = 0;
+let reverseType = false;
 
-const intervalId=setInterval(()=>{
-    if(!reverseType) {
-        italic.textContent = word.substring(0, characterIndex++);
-        if (characterIndex === word.length) reverseType = true;
-    } else {
-        italic.textContent = word.substring(0, characterIndex--);
-        if (characterIndex === 0) reverseType = false;
-    }
-}, 200);
+const intervalId = setInterval(() => {
+  if (!reverseType) {
+    italic.textContent = word.substring(0, characterIndex++);
+    if (characterIndex === word.length) reverseType = true;
+  } else {
+    italic.textContent = word.substring(0, characterIndex--);
+
+    if (characterIndex === 0) reverseType = false;
+  }
+}, 250);
